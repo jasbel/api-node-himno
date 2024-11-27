98940c872689a6e3c4ce367a2a5ae4da4d675db4
asbel.dev <asbel.elmec@gmail.com>
2024-11-26 20:49:29 -0400

    changes

--- src/app.ts
+++ src/app.ts
@@ -2,31 +2,7 @@ import express, { Request, Response } from 'express';
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 import { v4 as uuidv4 } from 'uuid';
-import { constrainedMemory } from 'process';
-
-type ID = string;
-type TNote = '_' | 'G|Sol' | 'A|La' | 'C|Do' | 'D|Re' | 'E|Mi' | 'B|Si' | 'F|Fa' | 'F#|Fa#' | 'C/D|Do-Re';
-
-// Interface for Song matches your specification
-interface IParagraph {
-  id: ID;
-  paragraph: string;
-  chorusPos: [positionOrId: number | ID, repeat?: number][];
-}
-
-interface IChoir {
-  id: ID;
-  choir: string,
-}
-
-interface ISong {
-  id: ID,
-  code: string,
-  title: string,
-  musicalNote: TNote | string,
-  paragraphs: IParagraph[] | string,
-  chorus: IChoir[],
-}
+import { ISong } from './types.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -41,7 +17,10 @@ async function initializeDatabase() {
   db = await open({
     filename: './songs.db',
     driver: sqlite3.Database
-  });
+  }).then(d => {
+    console.log('Conectado a al DB')
+    return d
+  }).catch(err => console.error('Error al conectar con SQLite:', err.message));
 
   // Create songs table if not exists
   await db.exec(`
@@ -60,13 +39,13 @@ async function initializeDatabase() {
 // Create a new song
 app.post('/songs', async (req: Request, res: Response) => {
   try {
-    const { 
-      num_song, 
-      title, 
-      description, 
-      musicalNote, 
-      paragraphs, 
-      chorus 
+    const {
+      num_song,
+      title,
+      description,
+      musicalNote,
+      paragraphs,
+      chorus
     } = req.body;
 
     const id = uuidv4();
@@ -78,15 +57,15 @@ app.post('/songs', async (req: Request, res: Response) => {
     `;
 
     const paragraphsString = JSON.stringify(paragraphs);
-    console.log({paragraphsString});
+    console.log({ paragraphsString });
 
     await db.run(query, [
-      id, 
-      num_song, 
-      title, 
-      description, 
-      musicalNote, 
-      paragraphsString, 
+      id,
+      num_song,
+      title,
+      description,
+      musicalNote,
+      paragraphsString,
       chorus
     ]);
 
@@ -101,14 +80,14 @@ app.get('/songs', async (req: Request, res: Response) => {
   try {
     let songs: ISong[] = await db.all('SELECT * FROM songs');
     songs.forEach((s, i) => {
-      const paragraphs = typeof s.paragraphs === 'string' 
-      ? JSON.parse(<string>s.paragraphs) 
-      : s.paragraphs;
+      const paragraphs = typeof s.paragraphs === 'string'
+        ? JSON.parse(<string>s.paragraphs)
+        : s.paragraphs;
       songs[i].paragraphs = paragraphs;
     });
     res.json(songs);
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching songs: ' + error,  });
+    res.status(500).json({ error: 'Error fetching songs: ' + error, });
   }
 });
 
@@ -129,13 +108,13 @@ app.get('/songs/:id', async (req: Request, res: Response) => {
 // Update a song
 app.put('/songs/:id', async (req: Request, res: Response) => {
   try {
-    const { 
-      num_song, 
-      title, 
-      description, 
-      musicalNote, 
-      paragraphs, 
-      chorus 
+    const {
+      num_song,
+      title,
+      description,
+      musicalNote,
+      paragraphs,
+      chorus
     } = req.body;
 
     const query = `
@@ -146,12 +125,12 @@ app.put('/songs/:id', async (req: Request, res: Response) => {
     `;
 
     const result = await db.run(query, [
-      num_song, 
-      title, 
-      description, 
-      musicalNote, 
-      paragraphs, 
-      chorus, 
+      num_song,
+      title,
+      description,
+      musicalNote,
+      paragraphs,
+      chorus,
       req.params.id
     ]);
 
